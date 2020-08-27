d9caa7d3b3467a20a23c40d66398417f5e678990
sara alamri <sara99.edu@gmail.com>
2020-08-27 20:29:09 +0300

    Initial commit

---
+++ app/src/main/java/sa/dev/sarara/black_font_design/MainActivity.java
@@ -0,0 +1,15 @@
+package sa.dev.sarara.black_font_design;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int name = 7;
+    }
+}
